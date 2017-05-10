ce491302a3b12da176c113e4d9b687ba5902fbb5
Bigsby <bigsby@live.com>
2017-05-11 00:32:50 +0100

    Add level page

--- docs/scripts/appBuilder.js
+++ docs/scripts/appBuilder.js
@@ -1,19 +1,19 @@
 module.exports = {
-    ProcessData: function(data) {},
-    RegisterComponents: function(app) {
+    ProcessData: function (data) { },
+    RegisterComponents: function (app) {
         app.component("home", {
             templateUrl: templatePath("home"),
-            controller: function(data) {
+            controller: function (data) {
                 this.games = data.games;
             }
         });
 
         app.component("flowGame", {
             templateUrl: templatePath("flowGame"),
-            controller: function($http, $stateParams) {
+            controller: function ($http, $stateParams) {
                 var vm = this;
                 vm.$params = $stateParams;
-                $http.get("data/" + vm.$params.id + ".json").then(function(response) {
+                $http.get("data/" + vm.$params.id + ".json").then(function (response) {
                     angular.extend(vm, response.data);
                 });
             }
@@ -21,65 +21,107 @@ module.exports = {
 
         app.component("flowPack", {
             templateUrl: templatePath("flowPack"),
-            controller: function($http, $stateParams) {
+            controller: function ($http, $stateParams) {
                 var vm = this;
                 vm.$params = $stateParams;
-                $http.get("data/" + vm.$params.id + ".json").then(function(response) {
+                $http.get("data/" + vm.$params.id + ".json").then(function (response) {
                     angular.extend(vm, response.data);
-                    vm.pack = response.data.packs.find(function(pack) {
+                    vm.pack = response.data.packs.find(function (pack) {
                         return pack.name == vm.$params.pack;
                     });
                 });
             }
         });
 
+        var pad = "000";
+
+        function padLevel(level) {
+            return pad.substring(0, pad.length - level.length) + level;
+        };
+
+        function imagePath(vm, level, tag) {
+            return "images/"
+                + vm.$params.id
+                + "/"
+                + vm.pack.name
+                + "/"
+                + vm.section.name
+                + "/"
+                + padLevel(level.number.toString())
+                + tag
+                + ".jpg";
+        };
+
         app.component("flowSection", {
             templateUrl: templatePath("flowSection"),
-            controller: function($http, $stateParams) {
+            controller: function ($http, $stateParams) {
                 var vm = this;
                 vm.$params = $stateParams;
-                var pad = "000";
 
-                function padLevel(level) {
-                    return pad.substring(0, pad.length - level.length) + level;
+                vm.levelImagePath = function (level) {
+                    return imagePath(vm, level, "");
                 };
 
-                function imagePath(level, tag){
-                    return "images/" 
-                        + vm.$params.id 
-                        + "/"
-                        + vm.pack.name
-                        + "/"
-                        + vm.section.name
-                        + "/"
-                        + padLevel(level.number.toString())
-                        + tag
-                        + ".jpg";
+                vm.levelImageInitialPath = function (level) {
+                    return imagePath(vm, level, "i");
                 };
 
-                vm.levelImagePath = function(level) {
-                    return imagePath(level, "");
-                };
-
-                vm.levelImageInitialPath = function(level) {
-                    return imagePath(level, "i");
-                };
-
-                $http.get("data/" + vm.$params.id + ".json").then(function(response) {
+                $http.get("data/" + vm.$params.id + ".json").then(function (response) {
                     angular.extend(vm, response.data);
-                    vm.pack = response.data.packs.find(function(pack) {
+                    vm.pack = response.data.packs.find(function (pack) {
                         return pack.name == vm.$params.pack;
                     });
 
                     if (vm.pack)
-                        vm.section = vm.pack.sections.find(function(section) {
+                        vm.section = vm.pack.sections.find(function (section) {
                             return section.name == vm.$params.section;
                         });
                 });
             }
         });
+
+        app.component("flowLevel", {
+            templateUrl: templatePath("flowLevel"),
+            controller: function ($http, $stateParams) {
+                var vm = this;
+                vm.$params = $stateParams;
+
+                vm.levelImagePath = function (level) {
+                    return imagePath(vm, level, "");
+                };
+
+                vm.levelImageInitialPath = function (level) {
+                    return imagePath(vm, level, "i");
+                };
+
+                $http.get("data/" + vm.$params.id + ".json").then(function (response) {
+                    angular.extend(vm, response.data);
+                    vm.pack = response.data.packs.find(function (pack) {
+                        return pack.name == vm.$params.pack;
+                    });
+
+                    if (vm.pack) {
+                        vm.section = vm.pack.sections.find(function (section) {
+                            return section.name == vm.$params.section;
+                        });
+
+                        if (vm.section) {
+                            vm.level = vm.section.levels.find(function (level) {
+                                return level.number == vm.$params.level;
+                            });
+
+                            if (vm.level){
+                                vm.initialImage = imagePath(vm, vm.level, "i");
+                                vm.solutionImage = imagePath(vm, vm.level, "");
+                            }
+                        }
+                    }
+
+                });
+            }
+        });
     },
-    RegisterStates: function(stateProvider) {
+    RegisterStates: function (stateProvider) {
         stateProvider.state({
             name: "home",
             url: "/",
@@ -102,6 +144,12 @@ module.exports = {
             name: "flowSection",
             url: "/flow/:id/:pack/:section",
             component: "flowSection"
+        });
+
+        stateProvider.state({
+            name: "flowLevel",
+            url: "/flow/:id/:pack/:section/:level",
+            component: "flowLevel"
         });
     }
 }
